1e10ae5af9ca9ba1bc7472dfb290ad6fb93dff4e
Nina Vasilieva <aninok92@gmail.com>
2024-03-15 15:55:33 +0200

    Rendering Dynamic Post Pages & Paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,21 +3,23 @@ import ReactMarkdown from 'react-markdown'
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
 
-const DUMMY_POST =  { 
-        slug: "getting-started-with-nextjs",
-        title: "Getting started with Next.js",
-        image: "getting-started-with-nextjs.jpg",
-        content: '# This ia a first post',
-        date:"2024-03-15"
-    }
+// const DUMMY_POST =  { 
+//         slug: "getting-started-with-nextjs",
+//         title: "Getting started with Next.js",
+//         image: "getting-started-with-nextjs.jpg",
+//         content: '# This ia a first post',
+//         date:"2024-03-15"
+//     }
 
 
-export default function PostContent(params) {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+export default function PostContent({post}) {
+  const {slug, image, title, content } = post  
+
+  const imagePath = `/images/posts/${slug}/${image}`
     return (
         <article className={classes.content}>
-            <PostHeader title={DUMMY_POST.title} image={imagePath} />
-            <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+            <PostHeader title={title} image={imagePath} />
+            <ReactMarkdown>{content}</ReactMarkdown>
         </article>
     )
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,34 @@
 import PostContent from "../../components/posts/post-detail/post-content"
+import { getPostData, getPostsFiles } from "../../lib/posts-util"
 
-function PostDetailPage(params) {
-    return <PostContent />
+function PostDetailPage({post}) {
+    return <PostContent post={post}/>
+}
+
+export function getStaticProps(context) {
+    const { params } = context
+    const { slug } = params
+
+    const postData = getPostData(slug)
+
+    return {
+        props: {
+            post: postData
+        },
+        revalidate: 600
+    }
+    
+}
+
+export function getStaticPaths(params) {
+    const postFileNames = getPostsFiles()
+    const slugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''))
+
+    return {
+        paths: slugs.map(slug => ({params: {slug}})),
+        fallback: false 
+    }
+    
 }
 
 export default PostDetailPage
