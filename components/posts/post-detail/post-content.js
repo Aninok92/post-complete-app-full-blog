55fad941f7b885c6e6e9aa428515f139bf064859
Nina Vasilieva <aninok92@gmail.com>
2024-03-15 15:57:14 +0200

    Rendering Images With The "Next Image" Component (From Markdown). Rendering Code Snippets From Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,7 +1,11 @@
 import ReactMarkdown from 'react-markdown'
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
+
 
 // const DUMMY_POST =  { 
 //         slug: "getting-started-with-nextjs",
@@ -13,13 +17,57 @@ import classes from "./post-content.module.css"
 
 
 export default function PostContent({post}) {
-  const {slug, image, title, content } = post  
+  const {slug, image, title, content } = post
+  
+  const customRenders ={
+    // img(image) {
+    //     return (
+    //         <Image
+    //             src={`/images/posts/${post.slug}/${image.src}`}
+    //             alt={image.alt}
+    //             width={600}
+    //             height={300}
+    //         />
+    //     )
+    // },
+    p(paragraph) {
+        const { node } = paragraph
+
+        if(node.children[0].tagName === 'img') {
+            const image = node.children[0]
+
+            return <div className={classes.image}>
+                  <Image
+                src={`/images/posts/${post.slug}/${image.properties.src}`}
+                alt={image.alt}
+                width={600}
+                height={300}
+            />
+            </div>
+    }
+    return <p>{paragraph.children}</p>
+},
+
+code(code) {
+    const { className, children } = code;
+    const language = className.split('-')[1];
+    return (
+      <SyntaxHighlighter
+        style={atomDark}
+        language={language}
+        children={children}
+      />
+    );
+  },
+
+
+  }
 
   const imagePath = `/images/posts/${slug}/${image}`
     return (
         <article className={classes.content}>
             <PostHeader title={title} image={imagePath} />
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <ReactMarkdown components={customRenders}>{content}</ReactMarkdown>
         </article>
     )
 }
