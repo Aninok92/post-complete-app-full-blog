94010515207931ed34afbec7ebcd1db9e7e6a8fd
Nina Vasilieva <aninok92@gmail.com>
2024-03-19 13:48:38 +0200

    Adding The Contact API Route, Sending Data From The Client To The API Route, Storing Messages With MongoDB In A Database,  Adding UI Feedback With Notifications

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,11 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+const PASSWORD = 'k7qH3VAqsw1jJgzB'
+
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method Not Allowed' });
+    }
     if (req.method === 'POST') {
         const { email, name, message } = req.body
   
@@ -17,6 +24,28 @@ export default function handler(req, res) {
 
     const newMessage = { email, name, message }
 
-    res.status(201).json({ message: 'Successfully stored message!', data: newMessage })
+    let client
+
+   try {
+    client = await MongoClient.connect(`mongodb+srv://aninok92:${PASSWORD}@cluster0.zuo7ept.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+   } catch(err) {
+    res.status(500).json({ message: err.message || 'Couldnt connect to database'})
+    return
+   }
+
+   const db = client.db('my-site')
+
+   try {
+    const result = await db.collection('messages').insertOne(newMessage)
+    newMessage.id = result.insertedId
+   } catch(err) {
+    client.close()
+    res.status(500).json({ message: err.message || 'Storing message failed' })
+    return
+   }
+
+   client.close()
+
+   res.status(201).json({ message: 'Successfully stored message!', data: newMessage })
 }
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import ContactForm from "../components/posts/contact/contact-form"
+import ContactForm from "../components/contact/contact-form"
 
 function ContactPage(params) {
     return <ContactForm/>
