b825d3f6d650f0538a9dc3f3fb39d92dd0f538ec
Nina Vasilieva <aninok92@gmail.com>
2024-03-19 18:59:11 +0200

    Create the NextJS Config File, Working With Environment Variables

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,20 +1,15 @@
 import ReactMarkdown from 'react-markdown'
 import Image from 'next/image';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css"
 
-
-// const DUMMY_POST =  { 
-//         slug: "getting-started-with-nextjs",
-//         title: "Getting started with Next.js",
-//         image: "getting-started-with-nextjs.jpg",
-//         content: '# This ia a first post',
-//         date:"2024-03-15"
-//     }
-
+SyntaxHighlighter.registerLanguage('js', js)
+SyntaxHighlighter.registerLanguage('css', css)
 
 export default function PostContent({post}) {
   const {slug, image, title, content } = post
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,5 @@
 import { MongoClient } from 'mongodb'
 
-const PASSWORD = 'k7qH3VAqsw1jJgzB'
-
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method Not Allowed' });
@@ -26,14 +24,16 @@ export default async function handler(req, res) {
 
     let client
 
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.zuo7ept.mongodb.net/?retryWrites=true&w=majority`
+
    try {
-    client = await MongoClient.connect(`mongodb+srv://aninok92:${PASSWORD}@cluster0.zuo7ept.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+    client = await MongoClient.connect(connectionString);
    } catch(err) {
     res.status(500).json({ message: err.message || 'Couldnt connect to database'})
     return
    }
 
-   const db = client.db('my-site')
+   const db = client.db(process.env.mongodb_database)
 
    try {
     const result = await db.collection('messages').insertOne(newMessage)
